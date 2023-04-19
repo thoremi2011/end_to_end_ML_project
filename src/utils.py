4175298ed58115ecbf5f29d6e3ed86d1f6515c00
Emelian Gurei <gureiemilian2@gmail.com>
2023-04-19 11:19:30 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from src.exception import CustomException
 from src.logger import logging
@@ -31,3 +32,10 @@ def tune_hyperparameters(X_train, y_train, model, params):
     except Exception as e:
         raise CustomException(e,sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
